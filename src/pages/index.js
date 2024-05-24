7fefb14a30e7fa7e8355a7860481f529b6d39baf
SevenDev <neolontu@gmail.com>
2024-05-24 14:23:04 -0400

    feat: implement item detail page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,25 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { getpokemonList } from '../services';
+import { getPokemonList } from '../services';
 import Loader from '../components/loader';
 
 function List() {
-  const [pokemonList, setpokemonList] = useState([])
+  const [pokemonList, setPokemonList] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetchData()
+    // eslint-disable-next-line
   }, [])
 
   const fetchData = async () => {
-    let response = await getpokemonList()
-    setpokemonList(response.results)
+    let response = await getPokemonList()
+    setPokemonList(response.results)
     setIsLoading(false)
   }
 
-  const getpokemonId = (pokemonUrl) => {
+  const getPokemonId = (pokemonUrl) => {
     return pokemonUrl.slice(0, pokemonUrl.length - 1).split("pokemon/")[1]
   }
 
@@ -34,7 +35,11 @@ function List() {
             className="flex flex-col items-center w-full sm:w-4/12 md:w-3/12 rounded-md overflow-hidden shadow-lg"
             to={`pokemon/${name}`}
           >
-            <img className="w-[96px] h-[96px] mb-3" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getpokemonId(url)}.png`} alt={name} />
+            <img
+              className="w-[96px] h-[96px] mb-3"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonId(url)}.png`}
+              alt={name}
+            />
             <div className="w-full px-6 py-4 bg-gray-200">
               <p className="font-bold text-xl uppercase text-center">{name}</p>
             </div>
