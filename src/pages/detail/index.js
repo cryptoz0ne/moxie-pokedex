4150eb2236d855a99c5a75e2cf78c3c910dce215
SevenDev <neolontu@gmail.com>
2024-05-27 04:56:30 -0400

    feat: handle bad request

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -18,21 +18,25 @@ function Detail() {
   }, [])
 
   const fetchData = async () => {
-    const response = await getPokemon(slug)
-    const tempPokemonData = {
-      id: response.id,
-      name: response.name,
-      exp: response.base_experience,
-      height: response.height,
-      weight: response.weight,
-      images: [],
-      stats: response.stats
+    try {
+      const response = await getPokemon(slug)
+      const tempPokemonData = {
+        id: response.id,
+        name: response.name,
+        exp: response.base_experience,
+        height: response.height,
+        weight: response.weight,
+        images: [],
+        stats: response.stats
+      }
+      Object.keys(response.sprites).forEach(sprite => {
+        if (response.sprites[sprite] && typeof response.sprites[sprite] === 'string')
+          tempPokemonData.images.push(response.sprites[sprite])
+      })
+      setPokemonData(tempPokemonData)    
+    } catch (e) {
+      console.log(e)
     }
-    Object.keys(response.sprites).forEach(sprite => {
-      if (response.sprites[sprite] && typeof response.sprites[sprite] === 'string')
-        tempPokemonData.images.push(response.sprites[sprite])
-    })
-    setPokemonData(tempPokemonData)    
     setIsLoading(false)
   }
 
@@ -52,56 +56,59 @@ function Detail() {
             <Loader /> <span className="text-gray-600">Loading...</span>
           </div>
         : <div className='flex grow justify-center align-center items-center w-full'>
-            <div className='flex flex-col md:flex-row rounded-md overflow-hidden shadow-lg'>
-              <Carousel
-                autoPlay
-                infiniteLoop
-                interval={1500}
-                showArrows={false}
-                showStatus={false}
-                showIndicators={false}
-                showThumbs={false}
-                swipeable={false}
-                className='p-6 max-w-[300px] flex items-center'
-              >
-                {pokemonData.images.map((image, index) => (
-                  <img key={index} src={image} alt={pokemonData.name} />
-                ))}
-              </Carousel>
-              <div className="flex flex-col p-6 bg-gray-200 w-full md:w-[300px]">
-                <div className="flex justify-between">
-                  <label>1. ID:</label>
-                  <span className="font-bold">#{pokemonData.id}</span>
-                </div>
-                <div className="flex justify-between">
-                  <label>2. Name:</label>
-                  <span className="font-bold">{pokemonData.name}</span>
-                </div>
-                <div className="flex justify-between">
-                  <label>3. Exp:</label>
-                  <span className="font-bold">{pokemonData.exp}</span>
-                </div>
-                <div className="flex justify-between">
-                  <label>4. Weight:</label>
-                  <span className="font-bold">{pokemonData.weight}</span>
-                </div>  
-                <div className="flex justify-between">
-                  <label>5. Height:</label>
-                  <span className="font-bold">{pokemonData.height}</span>
-                </div>
-                <div className="flex flex-col">
-                  <label>6. Stats</label>
-                  <div className="flex flex-col pl-7">
-                    {pokemonData.stats.map(({ base_stat, stat }, index) => (
-                      <div className="flex justify-between" key={index}>
-                        <label className="uppercase">{stat.name}</label>
-                        <span className="font-bold">{base_stat}</span>
-                      </div>
-                    ))}
+          {JSON.stringify(pokemonData) === "{}" // Not found
+            ? <p className="text-red-500 text-2xl text-center">Not Found</p>
+            : <div className='flex flex-col md:flex-row rounded-md overflow-hidden shadow-lg'>
+                <Carousel
+                  autoPlay
+                  infiniteLoop
+                  interval={1500}
+                  showArrows={false}
+                  showStatus={false}
+                  showIndicators={false}
+                  showThumbs={false}
+                  swipeable={false}
+                  className='p-6 max-w-[300px] flex items-center'
+                >
+                  {pokemonData.images.map((image, index) => (
+                    <img key={index} src={image} alt={pokemonData.name} />
+                  ))}
+                </Carousel>
+                <div className="flex flex-col p-6 bg-gray-200 w-full md:w-[300px]">
+                  <div className="flex justify-between">
+                    <label>1. ID:</label>
+                    <span className="font-bold">#{pokemonData.id}</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <label>2. Name:</label>
+                    <span className="font-bold">{pokemonData.name}</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <label>3. Exp:</label>
+                    <span className="font-bold">{pokemonData.exp}</span>
+                  </div>
+                  <div className="flex justify-between">
+                    <label>4. Weight:</label>
+                    <span className="font-bold">{pokemonData.weight}</span>
+                  </div>  
+                  <div className="flex justify-between">
+                    <label>5. Height:</label>
+                    <span className="font-bold">{pokemonData.height}</span>
+                  </div>
+                  <div className="flex flex-col">
+                    <label>6. Stats</label>
+                    <div className="flex flex-col pl-7">
+                      {pokemonData.stats.map(({ base_stat, stat }, index) => (
+                        <div className="flex justify-between" key={index}>
+                          <label className="uppercase">{stat.name}</label>
+                          <span className="font-bold">{base_stat}</span>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+          }
           </div>
       }
     </div>
